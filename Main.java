846918e669717800e2e7392f81841be4e2868379
categimperat <gene00choi@gmail.com>
2023-09-21 18:05:25 -0500

    formatting issues resolved

--- Main.java
+++ Main.java
@@ -13,10 +13,12 @@ class Person {
     }
 
 }
+
 public class Main {
     private static List<Double> resultsExp1 = new ArrayList<>();
     private static List<Double> resultsExp2 = new ArrayList<>();
     private static List<Double> resultsExp3 = new ArrayList<>();
+
     // Function to move the person, protocol 4 or 8
     private static void move(Person person, int protocol, int Dimension) {
         Person temp = new Person(person.xCoordinate, person.yCoordinate);
@@ -28,53 +30,53 @@ public class Main {
             direction = rand.nextInt(8);
 
         switch (direction) {
-            case 0:     //north
+            case 0: // north
                 if (person.yCoordinate < Dimension)
                     person.yCoordinate++;
                 break;
-            case 1:     //east
+            case 1: // east
                 if (person.xCoordinate < Dimension)
                     person.xCoordinate++;
                 break;
-            case 2:     //south
+            case 2: // south
                 if (person.yCoordinate > 0)
                     person.yCoordinate--;
                 break;
-            case 3:     //west
+            case 3: // west
                 if (person.xCoordinate > 0)
                     person.xCoordinate--;
                 break;
-            case 4:     //northeast
+            case 4: // northeast
                 if (person.yCoordinate < Dimension && person.xCoordinate < Dimension) {
                     person.yCoordinate++;
                     person.xCoordinate++;
                 }
                 break;
-            case 5:     //northwest
+            case 5: // northwest
                 if (person.yCoordinate < Dimension && person.xCoordinate > 0) {
                     person.yCoordinate++;
                     person.xCoordinate--;
                 }
                 break;
-            case 6:     //southeast
+            case 6: // southeast
                 if (person.yCoordinate > 0 && person.xCoordinate < Dimension) {
                     person.yCoordinate--;
                     person.xCoordinate++;
                 }
                 break;
-            case 7:     //southwest
+            case 7: // southwest
                 if (person.yCoordinate > 0 && person.xCoordinate > 0) {
                     person.yCoordinate--;
                     person.xCoordinate--;
                 }
                 break;
         }
-        if (protocol == 8 && person.xCoordinate == temp.xCoordinate && person.yCoordinate == temp.yCoordinate){
+        if (protocol == 8 && person.xCoordinate == temp.xCoordinate && person.yCoordinate == temp.yCoordinate) {
             move(person, protocol, Dimension);
         }
     }
 
-    //This function plays the game.
+    // This function plays the game.
     // This function actually executes the moves and gives the output.
     private static int playGame(Person person1, Person person2, int Dimension, int maxMoves, int protocol) {
 
@@ -82,16 +84,29 @@ public class Main {
 
         while (counter < maxMoves) {
             if (counter % 2 == 0) {
-                move(person1, protocol, Dimension);                        //It uses an if statement and take a modulus 2 of the move counter to rotate between each of the players so they alternate turns.
+                move(person1, protocol, Dimension); // It uses an if statement and take a modulus 2 of the move counter
+                                                    // to rotate between each of the players so they alternate turns.
             } else {
                 move(person2, protocol, Dimension);
             }
-            if (person1.xCoordinate == person2.xCoordinate && person1.yCoordinate == person2.yCoordinate)       //Then after the player has moved it checks if there has been a meeting, if there has it breaks out of the loop.
+            if (person1.xCoordinate == person2.xCoordinate && person1.yCoordinate == person2.yCoordinate)
+                // Then after
+                // the player
+                // has moved
+                // it checks
+                // if there
+                // has been a
+                // meeting, if
+                // there has
+                // it breaks
+                // out of the
+                // loop.
                 break;
-            counter++;                                                      //If there isn't a meeting then it adds to the counter and repeats the loop.
+            counter++; // If there isn't a meeting then it adds to the counter and repeats the loop.
         }
 
-        //This section checks if the meeting occurred or not and prints the appropriate message.
+        // This section checks if the meeting occurred or not and prints the appropriate
+        // message.
 
         if (counter < maxMoves)
             return ++counter;
@@ -110,7 +125,6 @@ public class Main {
 
         return data;
     }
-
 
     public static void main(String[] args) {
         int Dimension = 100;
@@ -143,11 +157,6 @@ public class Main {
         resultsExp3.add(high);
         resultsExp3.add(average);
 
-
-
-        }
-
-
     }
-}
 
+}
